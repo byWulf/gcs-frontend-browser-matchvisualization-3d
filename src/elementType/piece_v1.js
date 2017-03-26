c62e1a290c893fcceee517d7280fe9c8a4ccfc9b
michael_wolf <michael.wolf@atraveo.de>
2017-03-26 09:30:15 +0200

    piece_v1: can now be layed down flat onto the board (f.e. for carcassonne)

--- src/elementType/piece_v1.js
+++ src/elementType/piece_v1.js
@@ -27,6 +27,7 @@ class piece_v1 extends ElementTypeInterface {
             this.object.add(object);
         });
 
+        this.setLaying(data.isLaying, true);
         this.setCanBeMoved(data.canBeMovedTo);
     }
 
@@ -41,7 +42,43 @@ class piece_v1 extends ElementTypeInterface {
         }
     }
 
+    setLaying(isLaying, onCreate) {
+        if (onCreate) {
+            this.object.rotation.x = isLaying ? -90 * Math.PI / 180 : 0;
+        } else {
+            if (this.object.userData.rotationTween) this.object.userData.rotationTween.stop();
+
+            let rotationX = this.object.rotation.x;
+            let targetRotationX = isLaying ? -90 * Math.PI / 180 : 0;
+
+            let targetPositionY = 0;
+            if (isLaying) {
+                this.object.rotation.x = targetRotationX;
+                let objectPosition = this.visualization.sumParentPositions(this.object.parent);
+                let objectBox = new THREE.Box3().setFromObject(this.object);
+
+                targetPositionY = objectPosition.y - objectBox.min.y;
+
+                this.object.rotation.x = rotationX;
+            }
+
+            let diff = {rotation: rotationX, position: this.object.position.y};
+            this.object.userData.rotationTween = new TWEEN.Tween(diff)
+                .to({rotation: targetRotationX, position: targetPositionY}, 500)
+                .onUpdate(() => {
+                    this.object.rotation.x = diff.rotation;
+                    this.object.position.y = diff.position;
+
+                }).easing(TWEEN.Easing.Quintic.Out);
+
+            this.object.userData.rotationTween.start();
+        }
+    }
+
     onEvent(event, data) {
+        if (event === 'piece.layingChanged') {
+            this.setLaying(data.isLaying);
+        }
         if (event === 'piece.permissionChanged') {
             this.setCanBeMoved(data.canBeMovedTo);
         }
